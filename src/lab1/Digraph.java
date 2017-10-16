8858dafc3c0f15cac4857f70bafdd4fd02b63822
xiao tengzhao <1240836105@qq.com>
2017-10-16 13:40:23 +0800

    3rd commmit

--- src/lab1/Digraph.java
+++ src/lab1/Digraph.java
@@ -696,8 +696,9 @@ public class Digraph
 			return "-end-";
 		else
 		{	
-		
-		DigraphNode node = HeadNodeList.get(i);
+	
+
+	DigraphNode node = HeadNodeList.get(i);
 			int child=(int)(Math.random() * node.AdjPointNumber);
 			for(i=0; i<=child ; i++)
 			{
